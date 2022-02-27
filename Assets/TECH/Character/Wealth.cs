acff9c50db6464a3a31c620ef92b635daf4221c6
wDANDANw <yliu31@wpi.edu>
2022-02-26 22:17:50 -0500

    Simple Movement

--- Assets/TECH/Character/Wealth.cs
+++ Assets/TECH/Character/Wealth.cs
@@ -16,13 +16,13 @@ public class Wealth : MonoBehaviour
     public void Start()
     {
         // initialSpeed = GetComponent<ThirdPersonMovement>().speed;
-        initialSpeed = GetComponent<PlayerMovementInputController>().speed;
+        initialSpeed = GetComponent<PlayerMovement>().speed;
         animator = GetComponent<Animator>();
     }
 
     public void updateSpeed() {
         // GetComponent<ThirdPersonMovement>().speed = initialSpeed + coeff * currentWealth;
-        GetComponent<PlayerMovementInputController>().speed = initialSpeed + coeff * currentWealth;
+        GetComponent<PlayerMovement>().speed = initialSpeed + coeff * currentWealth;
         currentWealth += 1;
 
         animator.SetBool("Picking", true);
