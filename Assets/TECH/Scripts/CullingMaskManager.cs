97c385536c8a22e174fb8ca727311d171a963e5e
ThePiratePup <jdboucher@wpi.edu>
2022-04-09 12:46:10 -0400

    culling mask updates
    culling mask for mini map cam now updates when you have enough fixed objects.
    Note:
    Game settings values do not reset between runs in-editor. You can start the game with a ton of money and fixed objects already.

--- Assets/TECH/Scripts/CullingMaskManager.cs
+++ Assets/TECH/Scripts/CullingMaskManager.cs
@@ -8,53 +8,72 @@ public class CullingMaskManager : MonoBehaviour
     public Game settings;
     private int lastNumObjectsFixedVal = 0;
     
-    public bool playerVisible = false;
-    public bool visitedRoadTilesVisible = false;
-    public bool fixedObjectsVisible = false;
-    public bool unfixedObjectsVisible = false;
-    public bool coinsVisible = false;
-    public bool goalVisible = false;
-    public bool entireMapVisible = false;
+    public bool playerVisible = false; // Layer 8
+    public bool visitedRoadTilesVisible = false; // Layer 14
+    public bool fixedObjectsVisible = false; // Layer 15
+    public bool unfixedObjectsVisible = false; // Layer 16
+    public bool coinsVisible = false; // Layer 9
+    public bool goalVisible = false; // Layer 17
+    public bool entireMapVisible = false; // Layer 13
+
+
+    private int cullingmask1 = (16640);
+    private int cullingmask2 = (49408);
+    private int cullingmask3 = (114944);
+    private int cullingmask4 = (115456);
+    private int cullingmask5 = (246528);
+    private int cullingmask6 = (254720);
+
+    private int currentCullingMask = (0);
+    
+    
 
     public void UpdateMapVisibility(int objectsFixed)
     {
+
         if(objectsFixed >= 3)
         {
             playerVisible = true;
             visitedRoadTilesVisible = true;
+            currentCullingMask = cullingmask1;
         }
 
         if (objectsFixed >= 5)
         {
             fixedObjectsVisible = true;
+            currentCullingMask = cullingmask2;
         }
 
         if (objectsFixed >= 7)
         {
             unfixedObjectsVisible = true;
+            currentCullingMask = cullingmask3;
         }
 
         if (objectsFixed >= 10)
         {
             coinsVisible = true;
+            currentCullingMask = cullingmask4;
         }
 
         if (objectsFixed >= 15)
         {
             goalVisible = true;
+            currentCullingMask = cullingmask5;
         }
 
         if (objectsFixed >= 20)
         {
             entireMapVisible = true;
+            currentCullingMask = cullingmask6;
         }
 
-        UpdateCullingMask();
+        UpdateCullingMask(currentCullingMask);
     }
 
-    public void UpdateCullingMask()
+    public void UpdateCullingMask(int newCullingMask)
     {
-
+        camera.cullingMask |= newCullingMask;
     }
 
     void Update()
